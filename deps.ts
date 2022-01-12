b18955da885013f60c4c8b88e5bb75a740564add
Byytelope <shadhanm@gmail.com>
2022-01-12 20:16:37 +0500

    update mongo

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * from "https://deno.land/x/harmony@v2.4.0/mod.ts";
-export { MongoClient } from "https://deno.land/x/mongo@v0.28.0/mod.ts";
+export { MongoClient } from "https://deno.land/x/mongo@v0.29.0/mod.ts";
